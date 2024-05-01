14e1ba6b37f5e5fa0dde5ca53edeffe9d4df868d
Will Sturley <willsturley@yahoo.co.uk>
2024-05-01 20:28:47 +0100

    Weapon Sway Movement and Rotation

--- Guns/Assets/Scripts/WeaponSway.cs
+++ Guns/Assets/Scripts/WeaponSway.cs
@@ -5,31 +5,60 @@ namespace Owl
 {
 	public class WeaponSway : MonoBehaviour
 	{
-		[SerializeField] private Vector2 _Intensity;
-		[SerializeField] private Single _Smoothness = 1f;
+		[Header("Position"), SerializeField] private Vector2 _MovementAmount = new Vector2(0.01f, 0.01f);
+		[SerializeField] private Single _MaxMovementAmount = 0.1f;
+		[SerializeField] private Single _MovementSmoothness = 6f;
 
+		[Header("Rotation"), SerializeField] private Vector2 _RotationAmount;
+		[SerializeField] private Single _MaxRotationAmount;
+		[SerializeField] private Single _RotationSmoothness;
+		[Space, SerializeField] private Boolean _RotationX = true;
+		[SerializeField] private Boolean _RotationY = true;
+		[SerializeField] private Boolean _RotationZ = true;
+
+		private Vector3 _OriginPosition;
 		private Quaternion _OriginRotation;
 
 		private void Start()
 		{
+			_OriginPosition = transform.localPosition;
 			_OriginRotation = transform.localRotation;
 		}
 
 		private void Update()
 		{
-			UpdateSway();
+			Vector2 input = CalculateSway();
+
+			MoveSway(input);
+			TiltSway(input);
 		}
 
-		private void UpdateSway()
+		private void TiltSway(Vector2 input)
 		{
-			Vector2 input = PlayerInputHandler.PlayerLook;
+			Single xAdjustment = Mathf.Clamp(_MovementAmount.x * input.x, -_MaxMovementAmount, _MaxMovementAmount);
+			Single yAdjustment = Mathf.Clamp(_MovementAmount.y * input.y, -_MaxMovementAmount, _MaxMovementAmount);
 
-			Quaternion xAdjustment = Quaternion.AngleAxis(-_Intensity.x * input.x, Vector3.up);
-			Quaternion yAdjustment = Quaternion.AngleAxis(_Intensity.y * input.y, Vector3.right);
-			Quaternion targetRotation = _OriginRotation * xAdjustment * yAdjustment;
+			Vector3 targetPosition = new Vector3(xAdjustment, yAdjustment, 0);
 
-			Quaternion rotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime * _Smoothness);
-			transform.localRotation = rotation;
+			transform.localPosition = Vector3.Lerp(transform.localPosition, _OriginPosition + targetPosition, Time.deltaTime * _MovementSmoothness);
+		}
+
+		private void MoveSway(Vector2 input)
+		{
+			Single yAdjustment = Mathf.Clamp(input.x * _RotationAmount.x, -_MaxRotationAmount, _MaxRotationAmount);
+			Single xAdjustment = Mathf.Clamp(input.y * _MovementAmount.y, -_MaxRotationAmount, _MaxRotationAmount);
+
+			Single targetX = _RotationX ? -xAdjustment : 0;
+			Single targetY = _RotationY ? yAdjustment : 0;
+			Single targetZ = _RotationZ ? yAdjustment : 0;
+			Quaternion targetRotation = Quaternion.Euler(new Vector3(targetX, targetY, targetZ));
+
+			transform.localRotation = Quaternion.Slerp(transform.localRotation, _OriginRotation * targetRotation, Time.deltaTime * _RotationSmoothness);
+		}
+
+		private static Vector2 CalculateSway()
+		{
+			return -PlayerInputHandler.PlayerLook;
 		}
 	}
 }
