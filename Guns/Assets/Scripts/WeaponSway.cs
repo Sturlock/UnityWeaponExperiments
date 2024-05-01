d3c9848a1418a23e7865a5c3198507a8f77c4766
Will Sturley <willsturley@yahoo.co.uk>
2024-05-01 18:51:36 +0100

    Exposing Values

--- Guns/Assets/Scripts/WeaponSway.cs
+++ Guns/Assets/Scripts/WeaponSway.cs
@@ -5,10 +5,13 @@ namespace Owl
 {
 	public class WeaponSway : MonoBehaviour
 	{
-		[SerializeField] private Single _Intensity = 1.0f;
-		[SerializeField] private Single _Smoothness = 1.0f;
+		[SerializeField] private Vector2 _Intensity;
+		[SerializeField] private Single _Smoothness = 1f;
 
 		private Quaternion _OriginRotation;
+		[SerializeField] private Quaternion _Rotation;
+		[SerializeField] private Quaternion _XAdjustment;
+		[SerializeField] private Quaternion _YAdjustment;
 
 		private void Start()
 		{
@@ -24,11 +27,12 @@ namespace Owl
 		{
 			Vector2 input = PlayerInputHandler.PlayerLook;
 
-			Quaternion xAdjustment = Quaternion.AngleAxis(-_Intensity * input.x, Vector3.up);
-			Quaternion yAdjustment = Quaternion.AngleAxis(_Intensity * input.y, Vector3.right);
-			Quaternion targetRotation = _OriginRotation * xAdjustment * yAdjustment;
+			_XAdjustment = Quaternion.AngleAxis(-_Intensity.x * input.x, Vector3.up);
+			_YAdjustment = Quaternion.AngleAxis(_Intensity.y * input.y, Vector3.right);
+			Quaternion targetRotation = _OriginRotation * _XAdjustment * _YAdjustment;
 
-			transform.localRotation = Quaternion.Lerp(transform.localRotation , targetRotation, Time.deltaTime * _Smoothness);
+			_Rotation = Quaternion.Lerp(transform.localRotation , targetRotation, Time.deltaTime * _Smoothness);
+			transform.localRotation = _Rotation;
 		}
 	}
 }
